897160712c49ab9c26f1346494e8b24a1e69064d
K S Archish <165703412+Archish1962@users.noreply.github.com>
2026-04-22 19:11:44 +0530

    Update interest calculation with new values

--- app.java
+++ app.java
@@ -1,10 +1,10 @@
 public class app {
     public static void main(String[] args) {
-        int p = 1030;
-        int r = 3;
+        int p = 1502;
+        int r = 5;
         int t = 4;
 
-        int si = (p*r*t)/100;
+        double si = (p*r*t)/100;
 
         System.out.println(si);
     }
